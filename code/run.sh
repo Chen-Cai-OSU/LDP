94badf033f57de5786bbd702246d39654c183dce
Chen Cai <agoodweathercc>
2019-03-13 17:02:52 -0400

    clearn

--- code/run.sh
+++ code/run.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
+# replicate results for imdb_binary and imdb_multi using linear kernel
+
 ~/anaconda2/bin/python main.py -W ignore --dataset 'imdb_multi'  --n_bin 100 --norm_flag 'no'
 ~/anaconda2/bin/python main.py -W ignore --dataset 'imdb_binary'  --n_bin 70 --norm_flag 'no'
-
-#~/anaconda2/bin/python main.py --dataset 'reddit_binary'  --n_bin 100 --norm_flag 'yes'
-
-exit
-for dataset in  'reddit_12K' #'collab' 'reddit_5K' 'reddit_12K'
-do
-    for n_bin in 30 #30 #50 70 100
-    do
-        time ~/anaconda2/bin/python main.py --dataset $dataset  --n_bin $n_bin --norm_flag 'yes'
-        time ~/anaconda2/bin/python main.py --dataset $dataset  --n_bin $n_bin --norm_flag 'no'
-    done
-done
